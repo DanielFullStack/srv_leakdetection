2229efcf90f2e7b827b1e7a2064a4dcb588203fd
Daniel Pereira do Nascimento <daniel.nascimento@etipi.pi.gov.br>
2024-12-01 20:39:14 -0300

    fix: Correção de configurações Redis

--- src/main/java/com/aguas/srv_leakdetection/config/RedisConfig.java
+++ src/main/java/com/aguas/srv_leakdetection/config/RedisConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import com.aguas.srv_leakdetection.model.PressureReading;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, PressureReading> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, PressureReading> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
     }
